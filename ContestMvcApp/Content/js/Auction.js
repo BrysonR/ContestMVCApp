f548a6e888e67530c3cdc6904ea9cbc041950206
BrysonR <Bryson@BrysonReynolds.com>
2013-08-01 15:29:27 -0500

    Added more cool stuff to the styling and such and

--- ContestMvcApp/Content/js/Auction.js
+++ ContestMvcApp/Content/js/Auction.js
@@ -1,18 +1,30 @@
-﻿function AuctionViewModel () {
+﻿var CreateListingViewModel = function() {
+    var self = this;
+    
+    this.listTitle = ko.observable();
+
+    this.listCategory = ko.observable();
+};
+
+var AuctionViewModel = function () {
 
     var self = this;
 
     this.itemToList = ko.observable(false);
 
-    this.listTitle = ko.observable();
-    this.listCategory = ko.observable();
+    this.listingToAdd = ko.observable();
 
     this.createItem = function() {
         self.itemToList(true);
+        self.listingToAdd(new CreateListingViewModel());
     };
 
-    this.done = function() {
-        self.itemToList(false);
+    this.listingItems = ko.observableArray();
+
+    this.saveListing = function() {
+        self.listingItems.push(self.listingToAdd());
+        self.listingToAdd(null);
+        self.listingToAdd(false);
     };
 };
 
