82cdadfebe12b8eecaa998b44a91723905e0b7ca
BrysonR <Bryson@BrysonReynolds.com>
2013-08-01 10:34:04 -0500

    applied bindings and added popup on click

--- ContestMvcApp/Content/js/Auction.js
+++ ContestMvcApp/Content/js/Auction.js
@@ -1,1 +1,19 @@
-﻿
+﻿function AuctionViewModel () {
+
+    var self = this;
+
+    this.itemToList = ko.observable(false);
+
+    this.listTitle = ko.observable();
+    this.listCategory = ko.observable();
+
+    this.createItem = function() {
+        self.itemToList(true);
+    };
+
+    this.done = function() {
+        self.itemToList(false);
+    };
+};
+
+vm = new AuctionViewModel();
